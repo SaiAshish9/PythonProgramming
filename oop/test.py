d159a787f29209bdf1d8b1480f65452a35e8b3ce
SaiAshish9 <saiashish3760@gmail.com>
2020-09-19 15:17:05 +0530

    oops started

--- oop/test.py
+++ oop/test.py
@@ -1,5 +1,17 @@
  # imperative programming allows computer
  # to follow sequence of instructions
+# a=10;
+# b=20;
+# print((10).__add__(20))
 
+class A(object):
 
- 
+    def __init__(self, make, price):
+        self.make= make
+        self.price= price
+        self.on= False
+
+a=A("1",2)
+print(a.price)
+
+print("{0.make} {0.price}".format(a))
