b0c4c6a26b4b6b79fcb97de5569c590c47fe2503
SaiAshish9 <saiashish3760@gmail.com>
2020-09-21 12:37:02 +0530

    pipenv pypi pip venv updated and package uploaded

---
+++ pythonPackageIndex/test.py
@@ -0,0 +1,21 @@
+# pypi
+# pip
+# pip list
+# semantic representation
+# 2.20.1
+# major version.minor version.patches/bug fixes
+
+# python -m pip install requests==2.9.0 --user
+# python -m pip install requests==2.9.* --user
+# python -m pip uninstall requests
+
+# The Python Package Index, abbreviated
+# as PyPI and also known as the Cheese Shop
+# (a reference to the Monty Python's Flying '
+# 'Circus sketch "Cheese Shop"), is the official third-party software repository for Python.
+
+# Full form of PIP is Pip Installs Python or PIP Installs
+# Packages Alternatively, pip stands for
+# "preferred installer program".
+
+# package manager for python
---
+++ pythonPackageIndex/virtualenv.py
@@ -0,0 +1,23 @@
+
+# python -m venv env
+
+# virtual directory 
+# is a specific virtual env
+# where we have a specific
+# python interpreter 
+# as well as the packages that
+# the application needs
+
+# mac
+# source env/bin/activate
+
+# windows
+# source env/Scripts/activate
+
+# python -m pip install requests==2.9.*
+
+# deactivate
+
+
+# pipenv --venv
+# pipenv shell
